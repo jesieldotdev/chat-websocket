069c3fac0729b2fe5c1d1e0feaaa2956f4b8fda6
jesiel364 <jesiel364@gmail.com>
2023-11-15 21:26:29 -0400

    fix: error in websocket

--- server/index.js
+++ server/index.js
@@ -2,42 +2,36 @@ import express from 'express'
 import cors from 'cors'
 import http from 'http'
 import {Server} from 'socket.io'
+const app = express();
 
-const app = express()
-app.use(cors())
+app.use(cors());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors:{
-        origin: ['http://localhost:5001'],
-        methods: ['GET', 'POST']
-    }
-})
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log(`User connected: ${socket.id}`)
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room", (data) => {
-        socket.join(data)
-        console.log(`User with ID: ${socket.id} joinned room: ${data}`)
-    })
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-    socket.on("send_message", (data) => {
-        console.log(data)
-        socket.to(data.room).emit("receive_message", data)
-    })
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-    io.on("disconnect", (socket) => {
-        console.log(`User disconnected: ${socket.id}`)
-    })
-
-   
-})
-
-
-
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(5000, () => {
-    console.log('Listening in port 5000')
-})
+  console.log("SERVER RUNNING");
+});
