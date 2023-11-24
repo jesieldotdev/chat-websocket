7dda5cc80124bad41e1f88b6e57e09234f67ea75
jesieldotdev <jesiel364@gmail.com>
2023-11-24 16:55:28 -0400

    feat: routes

---
+++ client/src/pages/Chat/index.tsx
@@ -0,0 +1,110 @@
+import { useEffect } from "react";
+import * as S from "./style";
+import IndexViewController, { userProps } from "../../viewController";
+
+interface chatProps {
+  messageList: any;
+  username: string;
+  currentMessage: any;
+  setCurrentMessage: any;
+  sendMessage: any;
+  allMessages: any;
+  warnings: any;
+  users: userProps;
+}
+
+const Chat = () => {
+  const {
+    username,
+    setUsername,
+    room,
+    setRoom,
+    showChat,
+    setShowChat,
+    joinRoom,
+    socket,
+    currentMessage,
+    setCurrentMessage,
+    setMessageList,
+    messageList,
+    sendMessage,
+    allMessages,
+    rooms,
+    warnings,
+    users,
+  } = IndexViewController();
+
+  // Função para remover objetos duplicados com base na propriedade 'id'
+  function removerDuplicados(array, propriedade) {
+    return array.filter(
+      (obj, index, self) =>
+        index === self.findIndex((el) => el[propriedade] === obj[propriedade])
+    );
+  }
+
+  // Remover objetos duplicados com base na propriedade 'id'
+  let arraySemRepeticao = removerDuplicados(messageList, "message");
+
+  console.log(warnings);
+  console.log(allMessages);
+  console.log(users);
+  // allMessages.map((item) => {
+  //   ar
+  // })
+
+  return (
+    <S.ChatWindow>
+      <div className="chat-header">
+        <p>{(users && users[0]?.room) || "Live Chat"}</p>
+        <div className="users">
+          {users &&
+            users?.map((user: userProps) => (
+              <span key={user.user}>
+                {" "}
+                {user.user} {users[users.length - 1] !== user ? "," : null}
+              </span>
+            ))}
+        </div>
+      </div>
+      <div className="chat-body">
+        <div className="warnings">{warnings}</div>
+
+        {arraySemRepeticao &&
+          arraySemRepeticao.map((messageContent: any) => {
+            return (
+              <div
+                id="message"
+                className={username !== messageContent.author ? "other" : "you"}
+              >
+                <div className="message-content">
+                  {messageContent.author !== username ? (
+                    <p className="author">{messageContent.author}</p>
+                  ) : null}
+                  <p>{messageContent.message}</p>
+                </div>
+
+                <div className="message-meta">
+                  <p className="time">{messageContent.time}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Mensagem..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        <button className="btn" onClick={() => sendMessage()}>
+          &#9658;
+        </button>
+      </div>
+    </S.ChatWindow>
+  );
+};
+
+export default Chat;
---
+++ client/src/pages/Chat/style.ts
@@ -0,0 +1,194 @@
+import styled from "styled-components";
+
+export const ChatWindow = styled.div`
+  width: 100%;
+  /* max-width: 500px; */
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  overflow-y: hidden;
+  /* margin: 0px; */
+  height: 100vh;
+  
+
+  .chat-header {
+    background-color: #044842;
+    color: #fefefe;
+    /* display: flex; */
+    padding: 8px;
+    /* margin-bottom: auto; */
+    position: relative;
+    top: 0px;
+    font-weight: 700;
+  }
+
+  .chat-body {
+    min-height: 76vh;
+    /* min-height: 67vh; */
+
+    background: #171819;
+    margin: 0;
+    padding: 16px;
+    padding-bottom: 200px;
+    /* width: 100%; */
+    height: 100%;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    /* padding-bottom: 100px; */
+    margin-bottom: 100px;
+  }
+
+  #message {
+    border-radius: 8px;
+    width: fit-content;
+    max-width: 70%;
+    padding: 8px;
+    margin-top: 16px;
+    padding-top: 0;
+    padding-left: 8px;
+    margin-bottom: auto;
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.2;
+  }
+
+  .message-content {
+    /*padding: 2px;*/
+    width: fit-content;
+  }
+
+  .you {
+    justify-content: flex;
+    background-color: #128C7E;
+    margin-left: auto;
+    color: #fff;
+  }
+
+  .you .message-content {
+    justify-content: flex-start;
+  }
+
+  .you .message-meta {
+    justify-content: flex-start;
+    margin-left: 5px;
+  }
+
+  .other {
+    justify-content: flex-end;
+    background-color: #363636;
+    color: #fff;
+
+    .time {
+      height: 100%;
+      margin-top: 0px;
+      display: flex;
+      align-items: end;
+      color: grey;
+    }
+  }
+
+  .other .message-content {
+    justify-content: flex-end;
+  }
+
+  .other .message-meta {
+    justify-content: flex-end;
+    margin-right: 5px;
+  }
+
+  .message-meta .author {
+    margin-left: 10px;
+  }
+
+  .author {
+    font-weight: bold;
+    color: orange;
+  }
+
+  .chat-footer {
+    margin: 12px;
+    height: 70px;
+    /* border: 1px solid #263238; */
+    border-radius: 16px;
+    border-top: none;
+    /* overflow: hidden; */
+
+    position: sticky;
+    bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+    
+  }
+
+  input {
+    height: 50px;
+    display: flex;
+    width: 90vw;
+    /* flex: 85%; */
+    border: 0;
+    border-radius: 16px;
+    padding: 0 0.7em;
+    font-size: 1em;
+    margin-right: 8px;
+
+    outline: none;
+    font-family: "Open Sans", sans-serif;
+    background-color: #1e2122;
+    color: #fefefe;
+
+
+  }
+
+  .btn:hover {
+    color: #fff;
+  }
+
+  .btn {
+    border: 0;
+    display: flex;
+    width: 70px;
+    height: 64px;
+    align-items: center;
+    justify-content: center;
+    border-radius: 16px;
+    padding: 8px;
+    /* display: grid; */
+    /* place-items: center; */
+    cursor: pointer;
+    /* flex: 15%; */
+    height: 100%;
+    background-color: #075a52;
+    /* background: transparent; */
+    outline: none;
+    font-size: 25px;
+    color: #fefefe;
+    /* margin-right: 16px; */
+    margin-right: auto;
+  }
+  .hide {
+    opacity: 0 !important;
+  }
+
+  .time {
+    font-size: 12px;
+    margin-left: 8px;
+    /*position: relative;*/
+    /*top: 20px;*/
+    color: #cbcbcb;
+    /*background-color: red;*/
+    height: 100%;
+    margin-top: 0px;
+    display: flex;
+    align-items: end;
+  }
+
+  .users{
+    font-size: 11px;
+    font-weight: 300;
+  }
+
+  .warnings{
+    color: #fff;
+    text-align: center;
+  }
+`;
